1323a1fcf1ba859153f4717bfb12af4db284b8da
Ben Nguyen <93725294+nben2021@users.noreply.github.com>
2023-03-04 22:50:25 -0500

    Completed my first JavaScripts project

---
+++ main.js
@@ -0,0 +1,76 @@
+// Initialize variables for game state
+let roundsPlayed = 0;
+let huScore = 0;
+let pcScore = 0;
+let coChoice = coPick();
+let huChoice = huPick();
+
+// STEP 1: Generate computer choice
+function coPick(){
+    let num = Math.floor(Math.random()*3);
+
+    if(num === 0) {
+        return "Rock";
+    } else if(num === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+// STEP 2: Get human choice from user input
+document.addEventListener("DOMContentLoaded", () => {huPick();});
+function huPick(){
+    const buttons = document.querySelectorAll(".button");
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", (event) => {
+            let buttonContent = event.target.textContent;
+            let huChoice = buttonContent;
+            let coChoice = coPick();
+            document.getElementById("pcPick").textContent = "Computer pick: " + coChoice;
+            document.getElementById("myPick").textContent = "My pick: " + huChoice;
+            roundOne(huChoice, coChoice);
+        })
+    }
+}
+
+// STEP 3: Determine winner of each round
+function roundOne(huChoice, coChoice) {
+    roundsPlayed++;
+    // Determine winner of round and update scores
+    if(huChoice === "Rock" && coChoice === "Rock" ||
+        huChoice === "Paper" && coChoice === "Paper" ||
+        huChoice === "Scissors" && coChoice === "Scissors") {
+          document.getElementById("winner").textContent = "This round is draw";
+
+        // No score change for draw
+    } else if(huChoice === "Rock" && coChoice === "Scissors" ||
+        huChoice === "Paper" && coChoice === "Rock" ||
+        huChoice === "Scissors" && coChoice === "Paper") {
+        huScore++;
+        document.getElementById("winner").textContent = "This round winner is human";
+
+        // Increment human score for win
+    } else {
+        pcScore++;
+        document.getElementById("winner").textContent = "This round winner is computer";
+        // Increment computer score for win
+    }
+
+    if (roundsPlayed === 5) {
+      // Determine winner of game and display message
+      if (huScore > pcScore) {
+          document.getElementById("winner").textContent = "CONGRATS HUMAN BEAT COMPUTER!!!";
+      } else if (huScore < pcScore) {
+          document.getElementById("winner").textContent = "CONGRATS COMPUTER BEAT HUMAN!!!";
+      } else if (huScore === pcScore) {
+          document.getElementById("winner").textContent = "THE GAME END IN A TIE";
+      }
+      // Disable buttons after game ends
+      const buttons = document.querySelectorAll(".button");
+      buttons.forEach((button) => (button.disabled = true));
+  }
+    // Update score display
+    document.getElementById("myScore").textContent = "Human - " + huScore;
+    document.getElementById("computerScore").textContent = "Computer - " + pcScore;    
+}
