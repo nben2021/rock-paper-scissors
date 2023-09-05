4efd90994c70fa18b1f60105e653dc313d58e24a
Ben Nguyen <93725294+nben2021@users.noreply.github.com>
2023-09-04 21:56:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let pcScore = 0;
 let coChoice = coPick();
 let huChoice = huPick();
 
-// STEP 1: Generate computer choice
+
 function coPick(){
     let num = Math.floor(Math.random()*3);
 
@@ -16,7 +16,7 @@ function coPick(){
       }
 }
 
-// STEP 2: Get human choice from user input
+
 document.addEventListener("DOMContentLoaded", () => {huPick();});
 function huPick(){
     const buttons = document.querySelectorAll(".button");
@@ -32,31 +32,31 @@ function huPick(){
     }
 }
 
-// STEP 3: Determine winner of each round
+
 function roundOne(huChoice, coChoice) {
     roundsPlayed++;
-    // Determine winner of round and update scores
+    
     if(huChoice === "Rock" && coChoice === "Rock" ||
         huChoice === "Paper" && coChoice === "Paper" ||
         huChoice === "Scissors" && coChoice === "Scissors") {
           document.getElementById("winner").textContent = "This round is draw";
 
-        // No score change for draw
+        
     } else if(huChoice === "Rock" && coChoice === "Scissors" ||
         huChoice === "Paper" && coChoice === "Rock" ||
         huChoice === "Scissors" && coChoice === "Paper") {
         huScore++;
         document.getElementById("winner").textContent = "This round winner is human";
 
-        // Increment human score for win
+        
     } else {
         pcScore++;
         document.getElementById("winner").textContent = "This round winner is computer";
-        // Increment computer score for win
+        
     }
 
     if (roundsPlayed === 5) {
-      // Determine winner of game and display message
+      
       if (huScore > pcScore) {
           document.getElementById("winner").textContent = "CONGRATS HUMAN BEAT COMPUTER!!!";
       } else if (huScore < pcScore) {
@@ -64,11 +64,11 @@ function roundOne(huChoice, coChoice) {
       } else if (huScore === pcScore) {
           document.getElementById("winner").textContent = "THE GAME END IN A TIE";
       }
-      // Disable buttons after game ends
+      
       const buttons = document.querySelectorAll(".button");
       buttons.forEach((button) => (button.disabled = true));
   }
-    // Update score display
+    
     document.getElementById("myScore").textContent = "Human - " + huScore;
     document.getElementById("computerScore").textContent = "Computer - " + pcScore;    
 }
