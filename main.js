49e0631fdeb29d18cae234fe29879e9ecba4450b
Ben Nguyen <93725294+nben2021@users.noreply.github.com>
2023-09-15 18:27:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let roundsPlayed = 0;
 let huScore = 0;
 let pcScore = 0;
+let coChoice = coPick();
+let huChoice = huPick();
 
 // Function to randomly pick a choice for the computer
 function coPick(){
@@ -22,6 +24,7 @@ function huPick(){
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", (event) => {
             // Get choices for this round
+            let buttonContent = event.target.textContent;
             let huChoice = event.target.textContent;
             let coChoice = coPick();
             
@@ -38,36 +41,40 @@ function huPick(){
 // Function to determine the winner of a single round and update scores
 function roundOne(huChoice, coChoice) {
     roundsPlayed++;
+    // Determine winner of round and update scores
+    if(huChoice === "Rock" && coChoice === "Rock" ||
+        huChoice === "Paper" && coChoice === "Paper" ||
+        huChoice === "Scissors" && coChoice === "Scissors") {
+          document.getElementById("winner").textContent = "This round is draw";
 
-    // Determine winner and update scores
-    if(huChoice === coChoice) {
-        document.getElementById("winner").textContent = "This round is draw";
-    } else if((huChoice === "Rock" && coChoice === "Scissors") ||
-              (huChoice === "Paper" && coChoice === "Rock") ||
-              (huChoice === "Scissors" && coChoice === "Paper")) {
+        // No score change for draw
+    } else if(huChoice === "Rock" && coChoice === "Scissors" ||
+        huChoice === "Paper" && coChoice === "Rock" ||
+        huChoice === "Scissors" && coChoice === "Paper") {
         huScore++;
         document.getElementById("winner").textContent = "This round winner is human";
+
+        // Increment human score for win
     } else {
         pcScore++;
         document.getElementById("winner").textContent = "This round winner is computer";
+        // Increment computer score for win
     }
 
-    // Check if game over and declare overall winner
     if (roundsPlayed === 5) {
-        if (huScore > pcScore) {
-            document.getElementById("winner").textContent = "CONGRATS HUMAN BEAT COMPUTER!!!";
-        } else if (huScore < pcScore) {
-            document.getElementById("winner").textContent = "CONGRATS COMPUTER BEAT HUMAN!!!";
-        } else {
-            document.getElementById("winner").textContent = "THE GAME END IN A TIE";
-        }
-
-        // Disable buttons after game over
-        const buttons = document.querySelectorAll(".button");
-        buttons.forEach((button) => (button.disabled = true));
-    }
-
-    // Update displayed scores
+      // Determine winner of game and display message
+      if (huScore > pcScore) {
+          document.getElementById("winner").textContent = "CONGRATS HUMAN BEAT COMPUTER!!!";
+      } else if (huScore < pcScore) {
+          document.getElementById("winner").textContent = "CONGRATS COMPUTER BEAT HUMAN!!!";
+      } else if (huScore === pcScore) {
+          document.getElementById("winner").textContent = "THE GAME END IN A TIE";
+      }
+      // Disable buttons after game ends
+      const buttons = document.querySelectorAll(".button");
+      buttons.forEach((button) => (button.disabled = true));
+  }
+    // Update score display
     document.getElementById("myScore").textContent = "Human - " + huScore;
-    document.getElementById("computerScore").textContent = "Computer - " + pcScore;
+    document.getElementById("computerScore").textContent = "Computer - " + pcScore;    
 }
