cfa90617fd2fb98ee50dad2fd1e66aa9385607ff
Ben Nguyen <93725294+nben2021@users.noreply.github.com>
2023-09-15 18:28:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,38 @@
-// Initialize variables to keep track of the game state
+// Initialize variables for game state
 let roundsPlayed = 0;
 let huScore = 0;
 let pcScore = 0;
 let coChoice = coPick();
 let huChoice = huPick();
 
-// Function to randomly pick a choice for the computer
+// STEP 1: Generate computer choice
 function coPick(){
     let num = Math.floor(Math.random()*3);
+
     switch (num) {
         case 0: return "Rock";
         case 1: return "Paper";
         default: return "Scissors";
-    }
+      }
 }
 
-// Event listener to run huPick once the DOM is fully loaded
+// STEP 2: Get human choice from user input
 document.addEventListener("DOMContentLoaded", () => {huPick();});
-
-// Function to capture human player's choice and initiate a game round
 function huPick(){
     const buttons = document.querySelectorAll(".button");
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener("click", (event) => {
-            // Get choices for this round
             let buttonContent = event.target.textContent;
-            let huChoice = event.target.textContent;
+            let huChoice = buttonContent;
             let coChoice = coPick();
-            
-            // Display choices
             document.getElementById("pcPick").textContent = "Computer pick: " + coChoice;
             document.getElementById("myPick").textContent = "My pick: " + huChoice;
-
-            // Execute a round of the game
             roundOne(huChoice, coChoice);
         })
     }
 }
 
-// Function to determine the winner of a single round and update scores
+// STEP 3: Determine winner of each round
 function roundOne(huChoice, coChoice) {
     roundsPlayed++;
     // Determine winner of round and update scores
